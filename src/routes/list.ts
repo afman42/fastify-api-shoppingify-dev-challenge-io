5a9ab08a457a81ea62f6111012b06562a8a0eed3
afman42 <afifarman50@gmail.com>
2023-06-20 13:12:08 +0700

    format prettier

--- src/routes/list.ts
+++ src/routes/list.ts
@@ -1,41 +1,52 @@
 import { FastifyInstance } from "fastify";
-import { CreateListBody, allListJsonSchema, createListJsonSchema, deleteListJsonSchema, updateListJsonSchema } from "../schemas/list";
-import { allListHandler, createListHandler, deleteListHandler, updateListHandler } from "../controllers/list";
+import {
+  CreateListBody,
+  allListJsonSchema,
+  createListJsonSchema,
+  deleteListJsonSchema,
+  updateListJsonSchema,
+} from "../schemas/list";
+import {
+  allListHandler,
+  createListHandler,
+  deleteListHandler,
+  updateListHandler,
+} from "../controllers/list";
 
-export default async  function listsRoutes(app: FastifyInstance) {
-    app.get(
-        "/",
-        {
-            schema: allListJsonSchema
-        },
-        allListHandler
-    )
+export default async function listsRoutes(app: FastifyInstance) {
+  app.get(
+    "/",
+    {
+      schema: allListJsonSchema,
+    },
+    allListHandler
+  );
 
-    app.post<{
-        Body: CreateListBody,
-    }>(
-        "/create",
-        {
-            schema: createListJsonSchema
-        },
-        createListHandler
-    )
+  app.post<{
+    Body: CreateListBody;
+  }>(
+    "/create",
+    {
+      schema: createListJsonSchema,
+    },
+    createListHandler
+  );
 
-    app.patch<{
-        Body: CreateListBody,
-    }>(
-        "/update/:id",
-        {
-            schema: updateListJsonSchema
-        },
-        updateListHandler
-    )
+  app.patch<{
+    Body: CreateListBody;
+  }>(
+    "/update/:id",
+    {
+      schema: updateListJsonSchema,
+    },
+    updateListHandler
+  );
 
-    app.delete(
-        "/delete/:id",
-        {
-            schema: deleteListJsonSchema
-        },
-        deleteListHandler
-    )
+  app.delete(
+    "/delete/:id",
+    {
+      schema: deleteListJsonSchema,
+    },
+    deleteListHandler
+  );
 }
