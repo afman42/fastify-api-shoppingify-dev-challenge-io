5aeaa04aef384c6503953a50483502bf3e9d3002
afman42 <afifarman50@gmail.com>
2023-08-19 10:11:03 +0700

    push to repo

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,11 +1,11 @@
 // import { Pool } from "pg";
-import ws from 'ws';
-import { Pool, neonConfig } from '@neondatabase/serverless';
+import ws from "ws";
+import { Pool, neonConfig } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/node-postgres";
 import { env } from "../config/env";
-import * as schema from "./schema"
+import * as schema from "./schema";
 
-neonConfig.webSocketConstructor = ws; 
+neonConfig.webSocketConstructor = ws;
 
 const pool = new Pool({
   connectionString: env.DATABASE_CONNECTION,
